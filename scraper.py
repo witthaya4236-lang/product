79a946adfeab90b1a2874ab2765c547c05303a95
product_name <witthaya4236@gmail.com>
2026-05-08 23:23:46 +0700

    scraper.py

---
+++ scraper.py
@@ -0,0 +1,22 @@
+[
+    {
+        "id": 1,
+        "category": "JOKE",
+        "name": "คนอร์คัพโจ๊กรสกุ้ง32g Pack",
+        "bigc": 93.0,
+        "lotus": 95.0,
+        "seven": 99.0,
+        "cj": 92.0,
+        "pack_size": 6
+    },
+    {
+        "id": 2,
+        "category": "CEREAL",
+        "name": "เนสวิต้าซีเรียลธัญญาหารรสดั้งเดิม25gx12 Pack",
+        "bigc": 99.0,
+        "lotus": 119.0,
+        "seven": 99.0,
+        "cj": 105.0,
+        "pack_size": 12
+    }
+]
